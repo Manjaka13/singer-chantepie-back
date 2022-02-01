d6a6548e27599f8714b0568cc5c491e3fbaee0d1
Manjaka13 <manjaka.rajaonson@gmail.com>
2022-02-01 19:17:59 +0100

    add user sign in

--- src/api/user.js
+++ src/api/user.js
@@ -4,13 +4,33 @@
 
 const Express = require("express");
 const Database = require("../database");
+const Auth = require("../auth");
 const route = Express.Router();
 const path = "/user/";
+const auth = new Auth();
 
 // Get all users
 route.get("/", (req, res) => {
 	res.json({
-		data: "User list",
+		data: "User list"
+	});
+});
+
+// Sign in incoming user
+route.post("/sign", (req, res) => {
+	let token = null;
+	const user = req.body;
+	const database = new Database("users");
+	const authUser = database.get().filter(item =>
+		item.name === user.name &&
+		item.password === user.password
+	);
+	console.log("Incoming user ", user);
+	console.log("Found user ", authUser);
+	if(authUser.length > 0)
+		token = auth.sign(user);
+	res.json({
+		data: token
 	});
 });
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ require("dotenv").config();
 // Import necessary tools
 const Express = require("express");
 const cors = require("cors");
-const Auth = require("./auth");
 // Import APIs
 const user = require("./api/user");
-const secret = require("uuid").v4;
 
 const app = Express();
 const port = process.env.PORT || 3001;
@@ -18,21 +16,6 @@ app.use(Express.json());
 
 // Mount APIs
 app.use(user.path, user.route);
-
-// const auth = new Auth();
-// const u1 = {
-// 	name: "Manjaka",
-// 	password: "hello"
-// };
-// const u2 = {
-// 	name: "Manjaka2",
-// 	password: "hello"
-// };
-// let token = auth.sign(u1);
-// console.log("Token ", token);
-// console.log("-----------------------");
-// console.log("Verification ", auth.verify(u1, token));
-// console.log("Verification ", auth.verify(u2, token));
 
 // Default home route
 app.get("/", (req, res) => {
